9981b00e3b29367241f6de97b17d7c2625b34a1c
Mosa, Jan Marvin M <marvinmosa@gmail.com>
2021-09-20 01:47:16 +0800

    implement put a marker in the user's location

--- app/src/main/java/com/prototype/exam/ui/main/view/userDetail/UserDetailFragment.kt
+++ app/src/main/java/com/prototype/exam/ui/main/view/userDetail/UserDetailFragment.kt
@@ -7,19 +7,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.prototype.exam.App
 import com.prototype.exam.R
-import com.prototype.exam.data.model.forecast.ForecastItem
+import com.prototype.exam.data.model.Geo
+import com.prototype.exam.data.model.User
 import com.prototype.exam.databinding.FragmentUserDetailBinding
 import com.prototype.exam.ui.base.BaseFragment
 import com.prototype.exam.ui.main.view.UserActivity
 import com.prototype.exam.ui.main.viewModel.UserDetailViewModel
 import com.prototype.exam.utils.Constants.BUNDLE_LOCATION_ID
-import com.prototype.exam.utils.FormatterUtils.CURRENT_TEMPERATURE_FORMAT
-import com.prototype.exam.utils.FormatterUtils.HI_TEMPERATURE_FORMAT
-import com.prototype.exam.utils.FormatterUtils.LOW_TEMPERATURE_FORMAT
-import com.prototype.exam.utils.FormatterUtils.getTemperature
 import com.prototype.exam.utils.Status
 import com.prototype.exam.utils.ViewModelFactory
 import kotlinx.coroutines.launch
@@ -28,13 +29,14 @@ import javax.inject.Inject
 class UserDetailFragment : BaseFragment(R.layout.fragment_user_detail) {
 
     @Inject
-    lateinit  var viewModelFactory: ViewModelFactory
-    lateinit  var viewModel: UserDetailViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+    lateinit var viewModel: UserDetailViewModel
     private var viewBinding: FragmentUserDetailBinding? = null
     private val binding get() = viewBinding!!
     private var locationId: String? = null
-    private lateinit var mMap : GoogleMap
-    private var mapReady = false
+    private lateinit var map: GoogleMap
+    private var isMapReady = false
+    private var geo: Geo? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,7 +48,8 @@ class UserDetailFragment : BaseFragment(R.layout.fragment_user_detail) {
         viewBinding = FragmentUserDetailBinding.inflate(inflater, container, false)
         val view = binding.root
         locationId = arguments?.getString(BUNDLE_LOCATION_ID)
-       // viewModel.fetchForecast(locationId)
+        locationId?.let { viewModel.fetchData(it.toInt()) }
+
         setupUi()
         setupObservers()
         return view
@@ -58,17 +61,35 @@ class UserDetailFragment : BaseFragment(R.layout.fragment_user_detail) {
     }
 
     override fun setupUi() {
-        binding.btnFavorite.setOnClickListener {
-         //   viewModel.onToggleFavorite(locationId, binding.btnFavorite.isChecked)
+        setupMap()
+    }
+
+    private fun setupMap() {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            map = googleMap
+            isMapReady = true
+            updateMap()
+        }
+    }
+
+    private fun updateMap() {
+        if (isMapReady) {
+            geo?.let {
+                val sydney = LatLng(it.lat.toDouble(), it.lng.toDouble())
+                map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+                map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            }
         }
     }
 
     override fun setupObservers() {
-        viewModel.forecasts.observe(requireActivity(), {
+        viewModel.user.observe(requireActivity(), {
             it?.let { result ->
                 when (result.status) {
                     Status.SUCCESS -> {
-                        result.data?.let { response -> displayData(response) }
+                        result.data?.let { displayData(it) }
                     }
                     Status.ERROR -> {
                         Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
@@ -81,19 +102,13 @@ class UserDetailFragment : BaseFragment(R.layout.fragment_user_detail) {
         })
     }
 
-    private fun displayData(forecast: ForecastItem) {
-        binding.textCity.text = forecast.name
-        binding.textTemperature.text = resources.getString(
-            R.string.unit_celsius,
-            getTemperature(CURRENT_TEMPERATURE_FORMAT, forecast.main.temperature)
-        )
-        binding.textTempRange.text = resources.getString(
-            R.string.detail_hi_low_temp,
-            getTemperature(HI_TEMPERATURE_FORMAT, forecast.main.maxTemperature),
-            getTemperature(LOW_TEMPERATURE_FORMAT, forecast.main.minTemperature)
-        )
-        binding.textWeather.text = forecast.weatherList[0].weather
-        binding.btnFavorite.isChecked = forecast.favorite
+    private fun displayData(user: User) {
+        binding.textCity.text = user.name
+        binding.textTemperature.text = user.username
+        binding.textTempRange.text = user.company.catchPhrase
+        binding.textWeather.text = user.address.suite
+        geo = user.address.geo
+        updateMap()
     }
 
     override fun triggerErrorEvent(message: String) = lifecycleScope.launch {
--- app/src/main/java/com/prototype/exam/ui/main/viewModel/UserDetailViewModel.kt
+++ app/src/main/java/com/prototype/exam/ui/main/viewModel/UserDetailViewModel.kt
@@ -4,10 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.prototype.exam.data.model.User
-import com.prototype.exam.data.model.forecast.ForecastItem
 import com.prototype.exam.data.repository.UserRepository
 import com.prototype.exam.ui.base.BaseViewModel
-import com.prototype.exam.utils.NetworkHelper
 import com.prototype.exam.utils.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,36 +13,23 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
-class UserDetailViewModel @Inject constructor(
-    private val repository: UserRepository,
-    private val networkHelper: NetworkHelper
-) : BaseViewModel() {
+class UserDetailViewModel @Inject constructor(private val repository: UserRepository) :
+    BaseViewModel() {
 
-//    init {
-//        fetchData()
-//    }
+    private val _user = MutableLiveData<Result<User>>()
+    val user: LiveData<Result<User>>
+        get() = _user
 
-    private val forecast = MutableLiveData<Result<ForecastItem>>()
-    val forecasts: LiveData<Result<ForecastItem>>
-        get() = forecast
-
-    private val _users = MutableLiveData<Result<User>>()
-    val users: LiveData<Result<User>>
-        get() = _users
-
-    fun fetchData() {
+    fun fetchData(id: Int) {
         viewModelScope.launch {
-            if (!networkHelper.isNetworkConnected()) {
-                 forecast.postValue(Result.error(null, "No internet connection"))
-            }
+            _user.postValue(Result.loading(null))
+
             withContext(Dispatchers.IO) {
-
+                val item = repository.getUser(id)
+                _user.postValue(Result.success(item))
             }
-
         }
     }
-
-
 
 
 }
