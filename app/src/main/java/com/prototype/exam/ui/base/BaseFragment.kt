d36d4ade1997289d63e788703ee879565efd9816
Mosa, Jan Marvin M <marvinmosa@gmail.com>
2021-09-22 04:26:27 +0800

    added request permission in the baseFragment

--- app/src/main/java/com/prototype/exam/ui/base/BaseFragment.kt
+++ app/src/main/java/com/prototype/exam/ui/base/BaseFragment.kt
@@ -1,24 +1,37 @@
 package com.prototype.exam.ui.base
 
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
 
 
-abstract class BaseFragment(contentLayoutId: Int) : Fragment(contentLayoutId) {
+abstract class BaseFragment(contentLayoutId: Int) : Fragment(contentLayoutId), PermissionCallback {
 
     abstract fun setupUi()
     abstract fun setupObservers()
 
     sealed class SingleEvent {
-        data class  ErrorEvent(val message: String): SingleEvent()
+        data class ErrorEvent(val message: String) : SingleEvent()
     }
 
     protected val eventChannel = Channel<SingleEvent>()
     val eventFlow = eventChannel.receiveAsFlow()
 
-    abstract fun triggerErrorEvent(message:String): Job
+    abstract fun triggerErrorEvent(message: String): Job
+
+    internal val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                onUserGranted()
+            } else {
+                onUserDenied()
+            }
+        }
+}
+
+interface PermissionCallback {
+    fun onUserGranted() {}
+    fun onUserDenied() {}
 }
